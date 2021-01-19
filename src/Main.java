daee09ecd0a2f16f7cce8a89bf0c1ade880d996b
Yuliya Muraveva <dju5@ya.ru>
2021-01-19 10:53:25 +0300

    made bonus miles service

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        double ticketPrice = 1_999_999_999.99;
+        int miles = (int) (ticketPrice / 20);
+        System.out.println(miles);
+    }
+}
